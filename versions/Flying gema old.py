730f203c2d0c292e446e6d86899bf474d63513d7
a doodle <64034796+BURPIDOODLEDO@users.noreply.github.com>
2022-05-28 16:07:05 +0100

    Update Flying gema old.py

--- versions/Flying gema old.py
+++ versions/Flying gema old.py
@@ -704,7 +704,7 @@ def drawmenu():
 fpsClock = pygame.time.Clock()
 #							set up pygame window
 pygame.init()
-screen = pygame.display.set_mode((760, 480))
+screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("game... nothing else...")
 #							Create variables
 framerate = 60
